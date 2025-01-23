9f487bfbc80a3ad529092710b7724d0e528c21ec
AneeckaP <aneeckapallikonda@gmail.com>
2025-01-22 19:37:05 -0500

    Done

---
+++ PythonActivity3.py/PythonActivty3.py
@@ -0,0 +1,9 @@
+tree1=55.6
+tree2=32.9
+tree3=40.7
+tree4=65.1
+tree5=29.5
+sum=tree1+tree2+tree3+tree4+tree5
+print("the sum of all the heights of the trees is",sum)
+average=sum/5
+print("The average of all heights of the trees is",average)
