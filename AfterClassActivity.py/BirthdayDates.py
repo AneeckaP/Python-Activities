e20f04feba9d27b480f14d7bf2e827c177b74c62
AneeckaP <aneeckapallikonda@gmail.com>
2025-01-23 18:32:56 -0500

    Done

---
+++ AfterClassActivity.py/BirthdayDates.py
@@ -0,0 +1,10 @@
+mom="Mom's birthday is July 22"
+print(mom)
+dad="Dad's birthday is June 4 "
+print (dad)
+sister=" My sister's birthday is August 12 "
+print (sister)
+cousin1="Cousin1's birthday is April 23"
+print (cousin1)
+cousin2= "Cousin2's birthday is January 14"
+print (cousin2)
