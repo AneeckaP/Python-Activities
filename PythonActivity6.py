556fa8caa3a57b6c3502645d849ebd4aaec9e471
AneeckaP <aneeckapallikonda@gmail.com>
2025-02-05 20:04:18 -0500

    done

---
+++ PythonActivity6.py
@@ -0,0 +1,23 @@
+a=21
+b=26
+c=21
+d=25
+
+if a and b and c and d:
+    print("All the numbers have boolean value as True")
+else:
+    print("At leats one number has boolean value as False")
+a=0
+b=-4
+c=7
+d=18
+
+if a > 0 or b > 0:
+    print("Either of the number is greater then 0")
+else:
+    print("No number is greater than 0")
+
+if c>0 or d>0:
+    print("Either of the number is greater than 0")
+else: 
+    print("No number is greater than 0")
