264bbe534cb445294b7328ded6ba1597ac2ddb17
AneeckaP <aneeckapallikonda@gmail.com>
2025-02-05 19:08:00 -0500

    Done

---
+++ AfterClassActivity.py/SummerTimes.py
@@ -0,0 +1,9 @@
+temperature = int(input("Enter the temperature in Fahrenheit: "))
+if temperature >= 75:
+    print("It's suitable for wearing light clothing!")
+else:
+    print("It's not suitable for wearing light clothing.")
+if temperature >= 75:
+    print("it sure is hot outside!")
+else:
+    print("Oh well! It is not hot outside!")
