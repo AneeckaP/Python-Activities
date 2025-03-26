d9ee748807b40c34f8857d7c08016b0f1ad15768
AneeckaP <aneeckapallikonda@gmail.com>
2025-03-26 19:37:59 -0400

    d

---
+++ PythonActivy11.py
@@ -0,0 +1,22 @@
+import turtle
+
+turtle.Screen().bgcolor("Pink")
+Aneecka=turtle.Turtle()
+Aneecka.forward(100)
+Aneecka.left(120)
+Aneecka.forward(100)
+Aneecka.left(120)
+Aneecka.forward(100)
+
+Aneecka.penup()
+Aneecka.right(150)
+Aneecka.forward(50)
+Aneecka.pendown()
+Aneecka.right (90)
+Aneecka.forward(100)
+Aneecka.right(120)
+Aneecka.forward(100)
+Aneecka.right(120)
+Aneecka.forward(100)
+
+turtle.done()
