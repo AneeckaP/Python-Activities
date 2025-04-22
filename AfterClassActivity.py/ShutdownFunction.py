7457e82f8435cc9434f2c07dc4753a5813cd39ed
AneeckaP <aneeckapallikonda@gmail.com>
2025-04-22 13:34:29 -0400

    23456

--- AfterClassActivity.py/ShutdownFunction.py
+++ AfterClassActivity.py/ShutdownFunction.py
@@ -1,2 +1,8 @@
-import os
-os.system("shutdown /s /t 5") 
+answer= int(input("Do you need to shut down your computer"))
+if answer=="Yes" or "yes":
+    print("Your computer will shut down in 5 second")
+    import os
+    os.system("shutdown /s /t 5") 
+else:
+    print("Your computer will not be shut down")
+    
