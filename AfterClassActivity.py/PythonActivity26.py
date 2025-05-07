d3bc70f9d04129843ff4bc57f51a010cc9fb46db
AneeckaP <aneeckapallikonda@gmail.com>
2025-05-07 19:41:48 -0400

    g

---
+++ AfterClassActivity.py/PythonActivity26.py
@@ -0,0 +1,16 @@
+aneecka=("Python is done on the computer", True, 3.14, 14)
+print (aneecka)
+
+aneecka=(7,8,9,10,11)
+print(aneecka)
+aneecka=aneecka + (13,)
+print(aneecka)
+
+aneecka1=(20,40,60,80,100)
+print (aneecka1.count(40))
+
+aneecka=(8,8,8,8,8,)
+slice=aneecka[0:4]
+print(slice)
+slice=aneecka[:4]
+print(aneecka)
