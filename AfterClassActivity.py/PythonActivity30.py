0ba1d8a45a0253908aafd9f960ee7708756b9bde
AneeckaP <aneeckapallikonda@gmail.com>
2025-05-21 19:44:52 -0400

    y

---
+++ AfterClassActivity.py/PythonActivity30.py
@@ -0,0 +1,8 @@
+setx={"green","blue"}
+sety={"blue","yellow"}
+print("Original set elements:")
+print (setx)
+print (sety)
+print("\n The intersection of the two sets is: ")
+setz=setx.intersection(sety)
+print(setz)
