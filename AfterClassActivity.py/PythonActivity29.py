c7479f07e0f33fd56aa02b078adb3d4c0a648696
AneeckaP <aneeckapallikonda@gmail.com>
2025-05-21 19:36:32 -0400

    wertyui

---
+++ AfterClassActivity.py/PythonActivity29.py
@@ -0,0 +1,12 @@
+my_set={1,2,3}
+print(my_set)
+my_set={1.0, "Hello", (1,2,3)}
+print(my_set)
+my_set={1,2,3,4,2,3}
+print(my_set,"\n")
+num_set=set([0,1,3,4,5])
+print("This is the original set: ")
+print(num_set)
+num_set.pop()
+print("After removing the first element from the given set: ")
+print(num_set,"\n")
